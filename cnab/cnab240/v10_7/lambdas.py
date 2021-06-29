4786e91cdd8ce9149d44c1f334c0551ebc51b71c
Mariane Pastor <carolinep_16@hotmail.com>
2021-06-29 18:48:50 -0300

    Remove import outside top-level

--- cnab/cnab240/v10_7/lambdas.py
+++ cnab/cnab240/v10_7/lambdas.py
@@ -1,4 +1,5 @@
 # pylint: disable=global-statement
+import datetime
 import os
 
 
@@ -39,14 +40,10 @@ def get_field_G020():
 
 
 def get_field_G016():
-    import datetime
-
     return datetime.datetime.now().strftime("%d%m%Y")
 
 
 def get_field_G017():
-    import datetime
-
     return datetime.datetime.now().strftime("%H%M%S")
 
 
