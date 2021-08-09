5d6ce4539d0e47a586b6681b03003e974b0ed621
Thiago Paim <thiago.paim@vinta.com.br>
2021-08-09 19:10:14 -0300

    removing changes from next PR

--- providers/spreadsheet/models.py
+++ providers/spreadsheet/models.py
@@ -14,16 +14,6 @@ class RegistrationType(Enum):
     others = "Others"
 
 
-class BaseConfig:
-    allow_population_by_field_name = True
-
-    @classmethod
-    def alias_generator(cls, field: str) -> str:
-        if field not in cls._mapping:
-            raise Exception()
-        return cls._mapping[field]
-
-
 class SpreadsheetCompany(BaseModel):
     company_name: str
     registration_type = RegistrationType.cnpj
@@ -42,27 +32,6 @@ class SpreadsheetCompany(BaseModel):
     address_cep_complement: str
     address_city: str
     address_state: str
-
-    class Config(BaseConfig):
-        _mapping = {
-            "company_name": "* Nome da Empresa",
-            "registration_type": "* Tipo de Inscrição da Empresa",
-            "registration_number": "* Número de Inscrição da Empresa",
-            "bank_name": "* Nome do Banco",
-            "bank_code": "* Código do Convênio no Banco",
-            "bank_agency": "* Agência Mantenedora da Conta ",
-            "bank_agency_digit": "* Dígito Verificador da Agência",
-            "bank_account_number": "* Número da Conta Corrente",
-            "bank_account_digit": "* Dígito Verificador da Conta",
-            "bank_account_agency_digit": "* Dígito Verificador da Ag/Conta",
-            "address_location": "Logradouro (Nome da Rua, Av, Pça, Etc)",
-            "address_number": "Número (Número do Local)",
-            "address_complement": "Complemento (Casa, Apto, Sala, Etc)",
-            "address_cep": "CEP",
-            "address_cep_complement": "Complemento do CEP",
-            "address_city": "Nome da Cidade",
-            "address_state": "Sigla do Estado",
-        }
 
 
 class SpreadsheetEmployee(BaseModel):
@@ -84,27 +53,6 @@ class SpreadsheetEmployee(BaseModel):
     address_city: str
     address_state: str
 
-    class Config(BaseConfig):
-        _mapping = {
-            "name": "Nome do Favorecido",
-            "registration_type": "* Tipo de Inscrição do Favorecido",
-            "registration_number": "* Nº de Inscrição do Favorecido",
-            "bank_code": "Código do Banco do Favorecido",
-            "bank_agency": "* Ag. Mantenedora da Cta do Favor.",
-            "bank_agency_digit": "* Dígito Verificador da Agência",
-            "bank_account_number": "* Número da Conta Corrente",
-            "bank_account_digit": "* Dígito Verificador da Conta",
-            "bank_account_agency_digit": "* Dígito Verificador da AG/Conta",
-            "address_location": "Logradouro (Nome da Rua, Av, Pça, Etc)",
-            "address_number": "Número (Nº do Local)",
-            "address_complement": "Complemento (Casa, Apto, Etc)",
-            "address_district": "Bairro",
-            "address_cep": "CEP",
-            "address_cep_complement": "Complemento do CEP",
-            "address_city": "Nome da Cidade",
-            "address_state": "Sigla do Estado",
-        }
-
 
 class SpreadsheetPayment(BaseModel):
     employee_name: str
@@ -112,23 +60,8 @@ class SpreadsheetPayment(BaseModel):
     payment_amount: str
     payment_date: CNABDate
 
-    class Config(BaseConfig):
-        _mapping = {
-            "employee_name": "Funcionário",
-            "identification_number": "Nº do Docum. Atribuído p/ Empresa",
-            "payment_amount": "Valor do Pagamento",
-            "payment_date": "Data do Pagamento",
-        }
-
 
 class Spreadsheet(BaseModel):
     company: SpreadsheetCompany
     employees: List[SpreadsheetEmployee]
     payments: List[SpreadsheetPayment]
-
-    class Config(BaseConfig):
-        _mapping = {
-            "company": "Empresa",
-            "employees": "Funcionários",
-            "payments": "Pagamentos",
-        }
