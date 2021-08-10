2b5d7cee6d31c25b0ae8c8ff8d69a249d2311e0e
Thiago Paim <thiago.paim@vinta.com.br>
2021-08-10 19:56:25 -0300

    updating handler test

--- tests/providers/spreadsheet/test_spreadsheet_handler.py
+++ tests/providers/spreadsheet/test_spreadsheet_handler.py
@@ -1,3 +1,5 @@
+import json
+
 from providers.spreadsheet.handler import SpreadsheetHandler
 
 
@@ -6,8 +8,55 @@ class TestSpreadsheetHandler:
         input_filename = "./tests/fixtures/test_spreadsheet.xlsx"
         handler = SpreadsheetHandler(input_filename=input_filename)
 
-        assert (
-            handler.spreadsheet.company.company_name == " Vinta Servicos e Solucoes Tec"
-        )
-        assert handler.spreadsheet.employees[0].name == "Maria Fulana da Silva"
-        assert handler.spreadsheet.payments[0].payment_amount == "1000"
+        expected_json = {
+            "company": {
+                "company_name": " Vinta Servicos e Solucoes Tec",
+                "registration_number": "99999999000999",
+                "bank_name": "Banco Intermedium",
+                "bank_code": "77",
+                "bank_agency": "1",
+                "bank_agency_digit": "9",
+                "bank_account_number": "999999",
+                "bank_account_digit": "9",
+                "bank_account_agency_digit": "9",
+                "address_location": "Avenida Agamenon Magalhaes",
+                "address_number": "4318",
+                "address_complement": "sala 1801",
+                "address_cep": "52021",
+                "address_cep_complement": "170",
+                "address_city": "Recife",
+                "address_state": "PE",
+                "registration_type": "CGC/CNPJ",
+            },
+            "employees": [
+                {
+                    "name": "Maria Fulana da Silva",
+                    "registration_number": "99999999999",
+                    "bank_code": "77",
+                    "bank_agency": "0001",
+                    "bank_agency_digit": "9",
+                    "bank_account_number": "9999999",
+                    "bank_account_digit": "0",
+                    "bank_account_agency_digit": "0",
+                    "address_location": "Rua das Amelias",
+                    "address_number": "123",
+                    "address_complement": "1 Andar",
+                    "address_district": "Centro",
+                    "address_cep": "50050",
+                    "address_cep_complement": "000",
+                    "address_city": "Recife",
+                    "address_state": "PE",
+                    "registration_type": "CPF",
+                }
+            ],
+            "payments": [
+                {
+                    "employee_name": "Maria Fulana da Silva",
+                    "identification_number": "0001",
+                    "payment_amount": "1000",
+                    "payment_date": "11062021",
+                }
+            ],
+        }
+
+        assert json.loads(handler.spreadsheet.json()) == expected_json
