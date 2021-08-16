87c322ef84da6b7dc380787f56ea793253e2e805
Mariane Pastor <carolinep_16@hotmail.com>
2021-08-16 19:03:18 -0300

    Create API provider

---
+++ providers/api/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Response
+
+from cnab.payroll.models import Payroll
+from providers.spreadsheet.models import Spreadsheet
+
+
+app = FastAPI()
+
+
+@app.post("/parse_from_spreadsheet_data")
+async def parse_from_spreadsheet_data(spreadsheet_data: Spreadsheet):
+    spreadsheet = Spreadsheet.parse_obj(spreadsheet_data)
+    payroll = Payroll.parse_obj(spreadsheet.dict())
+
+    cnab_file = payroll.get_cnab_file()
+    response = cnab_file.as_fixed_width()
+
+    return Response(content=response, media_type="text/html")
