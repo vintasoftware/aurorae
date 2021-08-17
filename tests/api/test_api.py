63e9285e9cb97082ac3583cee17ed7c93b78057f
Mariane Pastor <carolinep_16@hotmail.com>
2021-08-17 11:21:58 -0300

    Fix tests

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -10,23 +10,23 @@ client = TestClient(app)
 @pytest.mark.usefixtures("spreadsheet_data")
 class TestAPI:
     def test_api_response_with_valid_payload(self, spreadsheet_data):
-        response = client.post("/parse_from_spreadsheet_data", json=spreadsheet_data)
+        response = client.post("/parse_from_spreadsheet", json=spreadsheet_data)
         assert response.status_code == 200
 
     def test_api_response_for_missing_company(self, spreadsheet_data):
         del spreadsheet_data["Empresa"]
 
-        response = client.post("/parse_from_spreadsheet_data", json=spreadsheet_data)
+        response = client.post("/parse_from_spreadsheet", json=spreadsheet_data)
         assert response.status_code == 422
 
     def test_api_response_for_missing_employee(self, spreadsheet_data):
         del spreadsheet_data["Funcionários"]
 
-        response = client.post("/parse_from_spreadsheet_data", json=spreadsheet_data)
+        response = client.post("/parse_from_spreadsheet", json=spreadsheet_data)
         assert response.status_code == 422
 
     def test_api_response_for_missing_payment(self, spreadsheet_data):
         del spreadsheet_data["Pagamentos"]
 
-        response = client.post("/parse_from_spreadsheet_data", json=spreadsheet_data)
+        response = client.post("/parse_from_spreadsheet", json=spreadsheet_data)
         assert response.status_code == 422
